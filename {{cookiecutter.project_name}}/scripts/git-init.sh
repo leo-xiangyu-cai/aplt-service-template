ae8ac2d139aa91f1eea1ea329dafc8fed73b928c
leo.cai <leo.cai@aplt.com>
2021-08-09 21:45:59 +1000

    update batect task: gitInit

--- {{cookiecutter.project_name}}/scripts/git-init.sh
+++ {{cookiecutter.project_name}}/scripts/git-init.sh
@@ -9,6 +9,15 @@ blue=$'\e[1;34m'
 end=$'\e[0m'
 
 git add .
+
+printf "%s\n" "${blue}Input your email:${end}"
+read -r email
+git config user.email "$email"
+
+printf "%s\n" "${blue}Input your name:${end}"
+read -r username
+git config user.email "$username"
+
 git commit -m "git init"
 
 printf "%s\n" "${blue}Input your remote git repo url:${end}"
